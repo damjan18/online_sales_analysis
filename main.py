5c6938987064a3cc6199bb6dbb8dd9cf25e69e2c
Damjan Pavlovic <pavlovic.damjan1828@gmail.com>
2025-02-15 11:05:20 +0100

    Added Cart class, tested in main.py

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 from product_manager import ProductManager
 from product import Product
+from cart import Cart
+import random
 
+ct = Cart()
 pm = ProductManager()
 
 pm.add_product(Product("Pepsi", 1.55, 40))
 pm.add_product(Product("Coca Cola", 1.75, 55))
 pm.add_product(Product("Doritos", 2.07, 25))
 pm.add_product(Product("Sinalco", 1.30, 35))
+pm.add_product(Product("Fanta", 1.70, 60))
+pm.add_product(Product("Sprite", 1.80, 43))
+
 
 pm.all_products()
 pm.total_value()
 
 pm.remove_product("Pepsi")
 pm.all_products()
+
+for i in range(0,3):
+    ct.add_to_cart(random.choice(pm.products))
+ct.view_cart_items()
+ct.total_cart_value()
