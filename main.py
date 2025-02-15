7f249e550b52038fd0e85ad2488e279d40fa774b
Damjan Pavlovic <pavlovic.damjan1828@gmail.com>
2025-02-15 11:09:51 +0100

    Changed main.py

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from product import Product
 
 pm = ProductManager()
 
-pm.add_product(Product("Pepsi", 1.55, 40))
-pm.add_product(Product("Coca Cola", 1.75, 55))
-pm.add_product(Product("Doritos", 2.07, 25))
-pm.add_product(Product("Sinalco", 1.30, 35))
+pm.add_product(Product("Pepsi", 1.55, 44))
+pm.add_product(Product("Coca Cola", 1.75, 53))
+pm.add_product(Product("Doritos", 2.07, 26))
+pm.add_product(Product("Sinalco", 1.30, 31))
 
-pm.all_products()
-pm.total_value()
+
 
 pm.remove_product("Pepsi")
-pm.all_products()
+
